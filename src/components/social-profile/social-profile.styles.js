8dae9af32037bc031936357c6f0bb3b52ac7f5f3
Alicja Witek <wituss97@gmail.com>
2022-05-26 03:33:52 +0200

    tryin to solve the problem with ul  visual

--- src/components/social-profile/social-profile.styles.js
+++ src/components/social-profile/social-profile.styles.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const SocialProf=styled.div `
-    max-width: 300px;
+    width: 300px;
     text-align: center;
     box-shadow: 0px 2px 9px 6px rgb(218, 218, 218);
     margin-top: 10px;
@@ -49,32 +49,26 @@ export const SocialLoc=styled.p `
 
 export const Stats=styled.ul `
     list-style: none;
+    height: 70px;
     padding: 0;
-    margin: 0;
     border-top: 1px solid #e7ecf2;
     background-color: rgb(197, 197, 197, 0.29);
     display: flex;
-    align-items: center;`;
+   `;
+   
 
 export const StatsLi=styled.li `
-    width: 100px;
-    height: 70px;
     display: flex;
-    align-items: center;
-    justify-content: center; 
+    width: 100px;
+    margin: 0;
+    justify-content: center;
     flex-direction: column;
     border-right: 1px solid #e7ecf2;
     background-color: #f7f7f7;
-    margin: 0;
-    padding: 0;
-   
     `;
 
 export const StatsLab=styled.span `
     color: #4d5967;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
     font-size: 12px;
     `;
 
@@ -82,10 +76,8 @@ export const StatsQ=styled.span `
     font-size: 15px;
     font-weight: 700;
     color: #5d5b5a;
-    padding-top: 5px;
-    align-items: center;
-    display: flex;
-    `
+    padding-top: 4px;
+    `;
 
 
     
